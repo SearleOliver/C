f1b5a463bff38a3c7d518dbb1b293e4c96f2e4f1
NOTuglyduckling <112571328+SearleOliver@users.noreply.github.com>
2026-05-21 14:23:46 +0200

    fin EX4 debut EX5

--- PAra/EX4.c
+++ PAra/EX4.c
@@ -124,30 +124,79 @@ int main(int argc, char **argv){
     printf("Resultat Problème 3\n");
     printf("Meilleure Utilitée : %d\n",S3[num_obj-1][capacity]);
 
-    double t_seq[100], t_para2[100], t_para4[100], t_para8[100],t_para16[100];
+    double t_seq_pb2[100], t_para2_pb2[100], t_para4_pb2[100], t_para8_pb2[100], t_para16_pb2[100];
+    double t_seq_pb3[100], t_para2_pb3[100], t_para4_pb3[100], t_para8_pb3[100], t_para16_pb3[100];
+
+    num_obj=0;
+    read_problem("pb2.txt");
+
+    int (*S2_seq)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S2_p2)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S2_p4)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S2_p8)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S2_p16)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
 
     for (int i = 0; i < 100; i++) {
-        start = omp_get_wtime(); sequentiel(weight,utility,S3); t_seq[i]   = omp_get_wtime() - start;
-        start = omp_get_wtime(); parallel(weight,utility,S3,2);    t_para2[i]  = omp_get_wtime() - start;
-        start = omp_get_wtime(); parallel(weight,utility,S3,4);   t_para4[i] = omp_get_wtime() - start;
-        start = omp_get_wtime(); parallel(weight,utility,S3,8);     t_para8[i]   = omp_get_wtime() - start;
-        start = omp_get_wtime(); parallel(weight,utility,S3,16);     t_para16[i]   = omp_get_wtime() - start;
+        start = omp_get_wtime(); sequentiel(weight,utility,S2_seq); t_seq_pb2[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S2_p2,2); t_para2_pb2[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S2_p4,4); t_para4_pb2[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S2_p8,8); t_para8_pb2[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S2_p16,16); t_para16_pb2[i] = omp_get_wtime() - start;
     }
 
-    double s1=0, s2=0, s3=0, s4=0, s5=0;
-    for (i = 0; i < 100; i++) { s1+=t_seq[i]; s2+=t_para2[i]; s3+=t_para4[i]; s4+=t_para8[i]; s5+=t_para16[i]; }
-    printf("Sequentielle      moyenne : %f\n", s1/100);
-    printf("Parallel 2        moyenne : %f\n", s2/100);
-    printf("Parallel 4        moyenne: %f\n", s3/100);
-    printf("Parallel 8        moyenne : %f\n", s4/100);
-    printf("Parallel 16       moyenne : %f\n", s5/100);
+    printf("Verification PB2\n");
+    printf("SEQ  : %d\n", S2_seq[num_obj-1][capacity]);
+    printf("PAR2 : %d\n", S2_p2[num_obj-1][capacity]);
+    printf("PAR4 : %d\n", S2_p4[num_obj-1][capacity]);
+    printf("PAR8 : %d\n", S2_p8[num_obj-1][capacity]);
+    printf("PAR16: %d\n", S2_p16[num_obj-1][capacity]);
+
+    num_obj=0;
+    read_problem("pb3.txt");
+
+    int (*S3_seq)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S3_p2)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S3_p4)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S3_p8)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+    int (*S3_p16)[capacity+1] = malloc(sizeof(int[capacity+1]) * num_obj);
+
+    for (int i = 0; i < 100; i++) {
+        start = omp_get_wtime(); sequentiel(weight,utility,S3_seq); t_seq_pb3[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S3_p2,2); t_para2_pb3[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S3_p4,4); t_para4_pb3[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S3_p8,8); t_para8_pb3[i] = omp_get_wtime() - start;
+        start = omp_get_wtime(); parallel(weight,utility,S3_p16,16); t_para16_pb3[i] = omp_get_wtime() - start;
+    }
+
+    printf("Verification PB3\n");
+    printf("SEQ  : %d\n", S3_seq[num_obj-1][capacity]);
+    printf("PAR2 : %d\n", S3_p2[num_obj-1][capacity]);
+    printf("PAR4 : %d\n", S3_p4[num_obj-1][capacity]);
+    printf("PAR8 : %d\n", S3_p8[num_obj-1][capacity]);
+    printf("PAR16: %d\n", S3_p16[num_obj-1][capacity]);
 
     FILE* f = fopen("resultats.csv", "w");
     if (f == NULL) { fprintf(stderr, "Erreur ouverture fichier CSV\n"); return EXIT_FAILURE; }
 
-    fprintf(f, "execution, sequentielle,para2,para4,para8,para16\n");
+    fprintf(f, "probleme,execution,sequentielle,para2,para4,para8,para16\n");
+
     for (i = 0; i < 100; i++)
-        fprintf(f, "%d,%.6f,%.6f,%.6f,%.6f,%.6f\n", i+1, t_seq[i], t_para2[i], t_para4[i], t_para8[i],t_para16[i]);
+        fprintf(f, "pb2,%d,%.6f,%.6f,%.6f,%.6f,%.6f\n",
+            i+1,
+            t_seq_pb2[i],
+            t_para2_pb2[i],
+            t_para4_pb2[i],
+            t_para8_pb2[i],
+            t_para16_pb2[i]);
+
+    for (i = 0; i < 100; i++)
+        fprintf(f, "pb3,%d,%.6f,%.6f,%.6f,%.6f,%.6f\n",
+            i+1,
+            t_seq_pb3[i],
+            t_para2_pb3[i],
+            t_para4_pb3[i],
+            t_para8_pb3[i],
+            t_para16_pb3[i]);
 
     fclose(f);
     printf("Résultats exportés dans resultats.csv\n");
@@ -156,5 +205,16 @@ int main(int argc, char **argv){
     free(S1);
     free(S2);
     free(S3);
+    free(S2_seq);
+    free(S2_p2);
+    free(S2_p4);
+    free(S2_p8);
+    free(S2_p16);
+
+    free(S3_seq);
+    free(S3_p2);
+    free(S3_p4);
+    free(S3_p8);
+    free(S3_p16);
     return EXIT_SUCCESS;
 }
